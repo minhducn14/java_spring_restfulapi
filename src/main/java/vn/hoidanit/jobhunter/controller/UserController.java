ddabffc9159d5adb3d45764764feca5fc7b10164
unknown <vunguyenminhduc1410@gmail.com>
2024-09-09 20:01:50 +0700

    Video 28

--- src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -5,7 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 import vn.hoidanit.jobhunter.domain.User;
 import vn.hoidanit.jobhunter.service.UserService;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,11 +21,17 @@ public class UserController {
     }
 
     // @GetMapping("/user/create")
-    @PostMapping("/user/create")
+    @PostMapping("/user")
     public User createNewUser(@RequestBody User user) {
 
         User userCreate = userService.handleCreateUser(user);
         return userCreate;
+    }
+
+    @DeleteMapping("/user/{user_id}")
+    public String deleteUser(@PathVariable("user_id") long id) {
+        userService.handleDeleteUser(id);
+        return "Delete user with id: " + id;
     }
 
 }
