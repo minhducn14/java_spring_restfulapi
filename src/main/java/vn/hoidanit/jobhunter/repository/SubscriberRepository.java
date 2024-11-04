b2203856c645c76fb4e972bb93cc28031875c4c1
unknown <vunguyenminhduc1410@gmail.com>
2024-11-04 13:59:08 +0700

    Video 130 thêm tính năng download và giao diện subscribers

--- src/main/java/vn/hoidanit/jobhunter/repository/SubscriberRepository.java
+++ src/main/java/vn/hoidanit/jobhunter/repository/SubscriberRepository.java
@@ -8,7 +8,9 @@ import vn.hoidanit.jobhunter.domain.Subscriber;
 
 @Repository
 public interface SubscriberRepository extends JpaRepository<Subscriber, Long>,
-                JpaSpecificationExecutor<Subscriber> {
+        JpaSpecificationExecutor<Subscriber> {
 
         boolean existsByEmail(String email);
+
+        Subscriber findByEmail(String email);
 }
